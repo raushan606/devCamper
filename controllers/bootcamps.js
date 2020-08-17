852329f598a22aa945dc8061ec5b623ab75c3c34
Raushan <raushankumar606@gmail.com>
2020-08-17 14:06:47 +0530

    update and delete bootcamp

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -6,10 +6,12 @@ const Bootcamp = require("../models/Bootcamp");
 exports.getBootcamps = async (req, res, next) => {
   try {
     const bootcamps = await Bootcamp.find();
-    if (!bootcamp) {
+    if (!bootcamps) {
       return res.status(400).json({ success: false });
     }
-    res.status(200).json({ success: true, data: bootcamps });
+    res
+      .status(200)
+      .json({ success: true, count: bootcamps.length, data: bootcamps });
   } catch (error) {
     res.status(400).json({ success: false });
   }
@@ -42,7 +44,6 @@ exports.createBootcamp = async (req, res, next) => {
   } catch (error) {
     res.status(400).json({
       success: false,
-      data: error.message,
     });
   }
 };
@@ -50,16 +51,38 @@ exports.createBootcamp = async (req, res, next) => {
 // @desc Update New Bootcamps
 // @routes PUT /api/v1/bootcamps/:id
 // @access Private
-exports.updateBootcamp = (req, res, next) => {
-  res.status(201).json({
-    name: "Raushan ",
-    todos: "Put Bootcamps",
-  });
+exports.updateBootcamp = async (req, res, next) => {
+  try {
+    const bootcamp = await Bootcamp.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+
+    if (!bootcamp) {
+      return res.status(400).json({ success: false });
+    }
+    res.status(200).json({ success: true, data: bootcamp });
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+    });
+  }
 };
 
 // @desc Delete New Bootcamps
 // @routes DELETE /api/v1/bootcamps/:id
 // @access Private
-exports.deleteBootcamp = (req, res, next) => {
-  res.status(201).json({ name: "Raushan ", todos: "Delete Bootcamps" });
+exports.deleteBootcamp = async (req, res, next) => {
+  try {
+    const bootcamp = await Bootcamp.findByIdAndDelete(req.params.id);
+
+    if (!bootcamp) {
+      return res.status(400).json({ success: false });
+    }
+    res.status(200).json({ success: true, data: bootcamp });
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+    });
+  }
 };
