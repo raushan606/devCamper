8602047803143dd879f57f9aeade5f327b49ca0e
Raushan <raushankumar606@gmail.com>
2020-08-18 16:27:07 +0530

    pagination

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -46,19 +46,41 @@ exports.getBootcamps = asyncHandler(async (req, res, next) => {
 
   //* Pagination
   const page = parseInt(req.query.page, 10) || 1;
-  const limit = parseInt(req.query.limit, 10) || 1;
-  const skip = (page - 1) * limit;
+  const limit = parseInt(req.query.limit, 10) || 25;
+  const startIndex = (page - 1) * limit;
+  const endIndex = page * limit;
+  const total = await Bootcamp.countDocuments();
 
-  query = query.skip(skip).limit(limit);
+  query = query.skip(startIndex).limit(limit);
 
   //* Executing query
   const bootcamps = await query;
-  if (!bootcamps) {
-    return res.status(400).json({ success: false });
+
+  //* Pagination Result
+  const pagination = {};
+
+  if (endIndex < total) {
+    pagination.next = {
+      page: page + 1,
+      limit,
+    };
   }
+
+  if (startIndex > 0) {
+    pagination.prev = {
+      page: page - 1,
+      limit,
+    };
+  }
+
   res
     .status(200)
-    .json({ success: true, count: bootcamps.length, data: bootcamps });
+    .json({
+      success: true,
+      count: bootcamps.length,
+      pagination,
+      data: bootcamps,
+    });
 });
 
 // @desc GET Single Bootcamp
