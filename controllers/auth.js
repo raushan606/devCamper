4acccaee1bd8b5aa7e6af2adca5f28daf5c31d27
Raushan <raushankumar606@gmail.com>
2020-08-22 18:05:14 +0530

    fogot password send email

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,3 +1,4 @@
+const sendEmail = require("../utils/sendEmail");
 const ErrorResponse = require("../utils/errorResponse");
 const User = require("../models/User");
 const asyncHandler = require("../middleware/async");
@@ -72,6 +73,34 @@ exports.forgotPassword = asyncHandler(async (req, res, next) => {
   const resetToken = user.getResetPasswordToken();
 
   await user.save({ validateBeforeSave: false });
+
+  //* Create reset URL
+  const resetUrl = `${req.protocol}://${req.get(
+    "host"
+  )}/api/v1/resetpassword/${resetToken}`;
+
+  const message = `You are receiving thhis email because you for someone else has reqeusted the reset of a password. Please make a PUT request to: \n\n ${resetUrl}`;
+
+  try {
+    await sendEmail({
+      email: user.email,
+      subject: "Password reset token",
+      message,
+    });
+
+    res.status(200).json({
+      success: true,
+      data: "Email Sent!!",
+    });
+  } catch (err) {
+    console.log(err);
+    user.resetPasswordToken = undefined;
+    user.resetPasswordExpire = undefined;
+
+    await user.save({ validateBeforeSave: false });
+
+    return next(new ErrorResponse("Email could not be sent", 500));
+  }
 
   res.status(200).json({
     success: true,
