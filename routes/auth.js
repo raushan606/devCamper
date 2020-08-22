cd2bdc633e38f771adb306b4a8a17f973fb4f8b6
Raushan <raushankumar606@gmail.com>
2020-08-22 18:17:56 +0530

    reset password

--- routes/auth.js
+++ routes/auth.js
@@ -4,6 +4,7 @@ const {
   login,
   getMe,
   forgotPassword,
+  resetPassword,
 } = require("../controllers/auth");
 const { protect } = require("../middleware/auth");
 
@@ -15,5 +16,6 @@ router.post("/login", login);
 
 router.get("/me", protect, getMe);
 router.post("/forgotpassword", forgotPassword);
+router.put("/resetpassword/:resettoken", resetPassword);
 
 module.exports = router;
