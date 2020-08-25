273e4ed6c7305ff2c2b4b0d621125a2a90bcb899
Raushan <raushankumar606@gmail.com>
2020-08-25 13:19:29 +0530

    xss protection

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ const errorHandler = require("./middleware/error");
 const fileUpload = require("express-fileupload");
 const cookieParser = require("cookie-parser");
 const mongoSanitize = require("express-mongo-sanitize");
+const helmet = require("helmet");
+const xss = require("xss-clean");
 
 //* Load env vars
 dotenv.config({ path: "./config/config.env" });
@@ -38,6 +40,12 @@ app.use(fileUpload());
 
 //* Sanitize data
 app.use(mongoSanitize());
+
+//* Set security headers
+app.use(helmet());
+
+//* Prevent XSS attacks
+app.use(xss());
 
 //* Set static folder
 app.use(express.static(path.join(__dirname, "public")));
